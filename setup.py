fdc70f9251491da24bffcc4bcf59899508e5ad12
Nathan Silverman <nzsilverman@gmail.com>
2020-07-10 10:48:47 -0700

    added a requirement for python version

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="scheduler",
     version="2.0.0",
+    author="Nathan Silverman",
+    author_email="nzsilver@umich.edu",
+    description="Climbing Club Carpool Scheduling Software",
     packages=find_packages(),
     install_requires=["gspread", "gspread-formatting", "oauth2client", "toml"],
     include_package_data=True,
     entry_points={"console_scripts": ["scheduler = scheduler.__main__:main",]},
     test_suite="nose2.collector.collector",
     tests_require=["nose2"],
+    python_requires=">=3.5",
 )
