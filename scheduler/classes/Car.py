5e090979405a106f9c2691da08be6633b39df56c
Nathan Silverman <nzsilverman@gmail.com>
2020-07-10 15:25:48 -0700

    removed type hint in Car

--- scheduler/classes/Car.py
+++ scheduler/classes/Car.py
@@ -4,7 +4,12 @@ class Car:
     """
 
     def __init__(self, driver: dict):
-        self.driver: dict = driver
+        self.driver = driver
         self.car_type: str = driver["car_type"]
         self.seats: int = driver["seats"]
         self.riders = list()
+    # def __init__(self, driver: dict):
+    #     self.driver: dict = driver
+    #     self.car_type: str = driver["car_type"]
+    #     self.seats: int = driver["seats"]
+    #     self.riders = list()
