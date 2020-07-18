75ebb62bad258f549a2bd26d9836cc7e8083bef9
Roberts Kalnins <rkalnins@umich.edu>
2020-07-18 18:39:41 -0400

    Format with yapf

--- docs/conf.py
+++ docs/conf.py
@@ -15,22 +15,18 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Michigan Climbing Club Carpool Scheduler'
 copyright = '2020, Nathan Silverman, Roberts Kalnins'
 author = 'Nathan Silverman, Roberts Kalnins'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx_rtd_theme"
-]
+extensions = ["sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,7 +35,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
